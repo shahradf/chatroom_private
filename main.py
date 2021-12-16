ee4216449cc14d930b8e99b66d67ecb79c1aa54e
shahradf <81154246+shahradf@users.noreply.github.com>
2021-12-16 16:25:42 +0330

    it word good but it hve some bug (added admin db and ...)

--- main.py
+++ main.py
@@ -22,35 +22,6 @@ def slow_print(vorody):
         sleep(10. / 100)
 
 
-def banner():
-    print('''
-    [] -> clear(-)
-    [] -> cmd(...)
-    [] -> cmd_r(...)
-    [] -> mute(...)
-    [] -> kick(...)
-    [] -> ban(...)
-    ''')
-
-
-def server_manager():
-    while True:
-        message_s = input('')
-
-
-user_login = input('username : ')
-password_login = input('password : ')
-if user_login == 'shahrad8b':
-    if password_login == 'login("/w let me in mosi")':
-        slow_print('welcome shahrad')
-    else:
-        print('password is wrong')
-        exit(0)
-else:
-    print('username is wrong')
-    exit(0)
-clear()
-
 db = SqliteDatabase('people.db')
 
 
@@ -58,6 +29,11 @@ class Person(Model):
     id = AutoField()
     username = CharField(max_length=24)
     password = CharField(max_length=256)
+    is_admin = BooleanField(default=False)
+    ban = BooleanField(default=False)
+    mute = BooleanField(default=False)
+
+    # nickname = CharField(max_length=30)
 
     class Meta:
         database = db
@@ -67,6 +43,21 @@ db.connect()
 db.create_tables([
     Person
 ])
+
+user_login = input('username : ')
+password_login = input('password : ')
+is_user_admin = Person.select().where((Person.username == user_login) & (Person.is_admin == True))
+if is_user_admin:
+    user = is_user_admin[-1]
+    if password_login == user.password:
+        slow_print(f'welcome {user_login}')
+    else:
+        print('password is wrong')
+        exit(0)
+else:
+    print('username is wrong')
+    exit(0)
+#clear()
 
 
 class Server:
@@ -96,15 +87,14 @@ class Server:
             password = c.recv(1024).decode()
             user_ip = c.recv(1024).decode()
 
-            user_db = Person.select().where(Person.username == username)
-            if user_db:
+            if user_db := Person.select().where(Person.username == username):
                 user_db = user_db[-1]
-                if password == user_db.password:
+                if password == user_db.password and user_db.ban == False:
                     print(f'new connection ip and username: {user_ip} -- {str(username)}')
                     self.broadcast(f'{username} connected')
 
                     self.username_lookup[c] = username
-
+                    print(self.username_lookup)
                     self.clients.append(c)
                     print(self.clients)
 
@@ -121,7 +111,25 @@ class Server:
     def handle_client(self, c, addr):
         while True:
             try:
-                msg = c.recv(1024)
+                username = self.username_lookup[c]
+                if user_db := Person.select().where(Person.username == username):
+                    user_db = user_db[-1]
+                    if user_db.mute == False:
+                        msg = c.recv(1024)
+                        if user_db.is_admin == True:
+                            msg_an = msg.decode().split()
+                            if msg_an[0] == '!kick':
+                                username = msg_an[1]
+                                user_select = self.username_lookup.keys()
+                                for user_f in user_select:
+                                    if self.username_lookup[user_f] == username:
+                                        user_f.shutdown(socket.SHUT_RDWR)
+                                        self.clients.remove(user_f)
+
+                    else:
+                        c.send('you\'r muted'.encode())
+                        c.shutdown(socket.SHUT_RDWR)
+                        self.clients.remove(c)
             except:
                 c.shutdown(socket.SHUT_RDWR)
                 self.clients.remove(c)
@@ -139,4 +147,3 @@ class Server:
 
 
 server = Server()
-banner()
